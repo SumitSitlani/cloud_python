7fe1242d1f0ee6b73a259a46b0cbbd7ac7aaca43
Sumit Sitlani <sumit@Sumits-MacBook-Pro-2.local>
2023-11-26 23:29:28 -0500

    made changes to admin pages

--- REMS/app/views.py
+++ REMS/app/views.py
@@ -4,32 +4,32 @@ from .models import Property,Location,Owner,Agent # Import your Property model
 
 def create_property(request):
     properties = Property.objects.select_related("owner","location").all()
-    #location = Location.objects.all()
-    #owner = Owner.objects.all()
+    location = Location.objects.all()
+    owner = Owner.objects.all()
     if request.method == 'POST':
         # Retrieve data from the POST request
         property_name = request.POST.get('propertyName')
         property_type = request.POST.get('propertyType')
         price = request.POST.get('propertyPrice')
         start_date = request.POST.get('StartDate')
-        #status = request.POST.get('status')
+        status = request.POST.get('propertyStatus')
         location_id = request.POST.get('propertySelect')  # Assuming location_id is submitted in the form
         owner_id = request.POST.get('ownerSelect')  # Assuming owner_id is submitted in the form
-        print(property_name,property_type,price,start_date,location_id,owner_id)
+        print(property_name,property_type,price,start_date,owner_id)
+        print(location_id, status)
         # Create a new Property object
         new_property = Property.objects.create(
             property_name=property_name,
             property_type=property_type,
             price=price,
             start_date=start_date,
-            #status=status,
+            status=status,
             location_id=location_id,  # Assign the location ID
             owner_id=owner_id  # Assign the owner ID
-            # Add other fields as needed
         )
 
         # Save the new property
-        #new_property.save()
+        new_property.save()
 
 
         # Redirect to the properties page or any other appropriate page
@@ -37,7 +37,7 @@ def create_property(request):
 
     else:
         # Handle GET request or render a form to create properties
-        return render(request, 'properties.html', {'properties': properties,}) #'location': location, 'owner':owner})  # Render a form for creating properties
+        return render(request, 'properties.html', {'properties': properties,'location': location, 'owner':owner})  # Render a form for creating properties
 
 def agnets(request):
     agnets = Agent.objects.all()
